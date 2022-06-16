c814309bb0a9de8d97a206ef82706aecf67069f8
Lê Ngọc Huế Trân <mk.ngochuetran2001@gmail.com>
2022-06-16 15:03:16 +0700

    comit

---
+++ Tuan5_LeNgocHueTran/Tuan5_LeNgocHueTran/Controllers/SachController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tuan5_LeNgocHueTran.Controllers
+{
+    public class SachController : Controller
+    {
+        // GET: Sach
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
