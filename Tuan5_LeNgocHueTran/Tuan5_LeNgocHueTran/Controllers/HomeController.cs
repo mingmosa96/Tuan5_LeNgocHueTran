f21dd07768814f1451d3e25326c12f5471be0634
Lê Ngọc Huế Trân <mk.ngochuetran2001@gmail.com>
2022-06-21 12:58:12 +0700

    ud

--- Tuan5_LeNgocHueTran/Tuan5_LeNgocHueTran/Controllers/HomeController.cs
+++ Tuan5_LeNgocHueTran/Tuan5_LeNgocHueTran/Controllers/HomeController.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Tuan5_LeNgocHueTran.Models;
+using PagedList;
 
 namespace Tuan5_LeNgocHueTran.Controllers
 {
     public class HomeController : Controller
     {
         MyDataDataContext data = new MyDataDataContext();
-        public ActionResult Index()
+        public ActionResult Index(int ? page)
         {
-            var all_sach = from s in data.Saches select s;
-            return View(all_sach);
+            if (page == null) page = 1;
+            var all_sach = (from s in data.Saches select s).OrderBy(m => m.masach);
+            int pageSize = 6;
+            int pageNum = page ?? 1;
+
+            return View(all_sach.ToPagedList(pageNum,pageSize));
         }
 
         public ActionResult About()
