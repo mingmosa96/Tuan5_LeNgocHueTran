7ea96126d0a16742af008164eee4f876d43d08c5
Lê Ngọc Huế Trân <mk.ngochuetran2001@gmail.com>
2022-06-22 08:14:21 +0700

    cm

--- Tuan5_LeNgocHueTran/Tuan5_LeNgocHueTran/Controllers/HomeController.cs
+++ Tuan5_LeNgocHueTran/Tuan5_LeNgocHueTran/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Tuan5_LeNgocHueTran.Controllers
         {
             if (page == null) page = 1;
             var all_sach = (from s in data.Saches select s).OrderBy(m => m.masach);
-            int pageSize = 6;
+            int pageSize = 2;
             int pageNum = page ?? 1;
 
             return View(all_sach.ToPagedList(pageNum,pageSize));
